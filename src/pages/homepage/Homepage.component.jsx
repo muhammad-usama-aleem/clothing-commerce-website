185d244bd2c0d2e2abe6db4d73f8058d5226c9f6
muhammad-usama-aleem <you@example.com>
2020-11-11 09:58:37 +0500

    adding missig files

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory.component.jsx';
+import './homepage.styles.scss';
+
+
+export default function HomePage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
