4b90d97ed6bf0dbdbed123aa4ab62541da1926ca
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2020-11-11 15:54:34 +0500

    adding a component and collection page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/Homepage.component.jsx';
+import Shop from './pages/shop/Shop.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
