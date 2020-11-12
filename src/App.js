062cc7d921eb911b3a3520638ee357a023ed0de5
muhammad-usama-aleem <you@example.com>
2020-11-12 11:22:22 +0500

    adding sign in and sign up page with some components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/Homepage.component.jsx';
-import Shop from './pages/shop/Shop.component.jsx';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import HomePage from './pages/homepage/Homepage.component';
+import Shop from './pages/shop/Shop.component';
+import Header from './components/header/Header.component';
+import SignInAndSignUpPage from './pages/Sign-In-Up/Sign-In-Up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
