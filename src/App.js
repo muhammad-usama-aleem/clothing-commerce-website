9b81548cea39357a920619e0f400bf849258b425
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2020-11-17 10:49:07 +0500

    email/password sign-up authentication firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/Homepage.component';
 import Shop from './pages/shop/Shop.component';
 import Header from './components/header/Header.component';
 import SignInAndSignUpPage from './pages/Sign-In-Up/Sign-In-Up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({ 
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({currentUser : userAuth});
     });
   }
 
